2b59d7f4946910ecb45ea8cbaca363a7df00d088
avaneesh93 <reddyavaneesh@gmail.com>
2017-11-09 13:53:58 -0500

    Adding tests

---
+++ tests/test_parsing.py
@@ -0,0 +1,12 @@
+import unittest
+from elena.parse import *
+
+
+class TestParsing(unittest.TestCase):
+    def test_parseCheck(self):
+        nodeStorage = parse("/Users/avaneesh/amherst")
+        self.assertEqual(len(nodeStorage.nodeMap), 45071)
+
+
+if __name__ == '__main__':
+    unittest.main()
