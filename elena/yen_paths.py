bccd3ef7804281d62172843c2d49cb80fe1806dd
avaneesh93 <reddyavaneesh@gmail.com>
2017-11-10 05:38:56 -0500

    Implemented A* and Yen algorithms

--- elena/yen_paths.py
+++ elena/yen_paths.py
@@ -2,12 +2,26 @@ from geopy.distance import vincenty
 from heapq import *
 
 
+def calculate_cost(nodeStorage, path):
+    sum = 0
+    for i in range(0, len(path) - 1):
+        node1 = nodeStorage.get_node(path[i])
+        node2 = nodeStorage.get_node(path[i+1])
+        sum += vincenty((node1.lat, node1.lng), (node2.lat, node2.lng)).meters
+    return sum
+
+
 def heuristic(node1, node2):
     return vincenty((node1.lat, node1.lng), (node2.lat, node2.lng)).meters
 
 
 # A-star implementation
-def get_a_star_path(nodeStorage, id1, id2):
+def get_a_star_path(nodeStorage, id1, id2, excluded_nodes=None, excluded_edges=None):
+    if excluded_nodes is None:
+        excluded_nodes = set([])
+    if excluded_edges is None:
+        excluded_edges = set([])
+
     frontier = []
     heappush(frontier, (0, id1))
     backtrace = {id1: None}
@@ -23,6 +37,11 @@ def get_a_star_path(nodeStorage, id1, id2):
 
         for next in nodeStorage.get_neighbors(cur[1]):
             next_id = next.id
+            if next_id in excluded_nodes:
+                continue
+            if (cur[1], next_id) in excluded_edges or (next_id, cur[1]) in excluded_edges:
+                continue
+
             new_cost = cost[cur[1]] + next.distance
             if next_id not in cost or new_cost < cost[next_id]:
                 cost[next_id] = new_cost
@@ -43,4 +62,53 @@ def get_a_star_path(nodeStorage, id1, id2):
 
 
 def get_shortest_paths(nodeStorage, id1, id2, dist):
-    pass
+    shortest_path, shortest_cost = get_a_star_path(nodeStorage, id1, id2)
+    if shortest_path is None:
+        return None
+
+    A = [(shortest_path, shortest_cost)]
+    B = []
+
+    print("Shortest path = {}".format(shortest_path))
+    print("Shortest path cost = {}".format(shortest_cost))
+
+    while True:
+        k = len(A)
+        cur_path = A[k - 1][0]
+        for i in range(0, len(cur_path) - 1):
+            spur_node = cur_path[i]
+            root_path = cur_path[:i + 1]
+            root_cost = calculate_cost(nodeStorage, root_path)
+
+            excluded_nodes = set([])
+            excluded_edges = set([])
+
+            for path, cost in A:
+                if i + 1 < len(path) and path[:i + 1] == root_path:
+                    excluded_edges.add((path[i], path[i + 1]))
+
+            for root_path_node in root_path:
+                if not root_path_node == spur_node:
+                    excluded_nodes.add(root_path_node)
+
+            spur_path, spur_cost = get_a_star_path(nodeStorage, spur_node, id2, excluded_nodes, excluded_edges)
+            if spur_path is None:
+                continue
+
+            total_path = root_path + spur_path[1:]
+            total_path_cost = spur_cost + root_cost
+
+            if (total_path, total_path_cost) not in B:
+                B.append((total_path, total_path_cost))
+
+        if len(B) == 0:
+            break
+
+        B.sort(key=lambda tup: tup[1], reverse=True)
+        if B[-1][1] > dist:
+            break
+
+        new_path_cost = B.pop()
+        A.append(new_path_cost)
+
+    return A
--- tests/test_paths.py
+++ tests/test_paths.py
@@ -8,10 +8,18 @@ class TestPaths(unittest.TestCase):
     def setUpClass(cls):
         cls.nodeStorage = parse("/Users/avaneesh/amherst")
 
+    def test_calculate_cost(self):
+        path = [66677654, 66754238, 66711385, 66732536, 66718935, 66769489, 66701422, 66710693, 66709586, 66727122, 66689101, 66604339, 61793182, 61791707]
+        dist = calculate_cost(self.nodeStorage, path)
+        print(dist)
+
     def test_a_star(self):
         path, cost = get_a_star_path(self.nodeStorage, 61791707, 66604339)
-        print("Path = {}".format(path))
-        print("Cost = {}".format(cost))
+        self.assertEqual([61791707, 61793182, 66604339], path)
+
+    def test_yen(self):
+        A = get_shortest_paths(self.nodeStorage, 66677654, 61791707, 200)
+        print(A)
 
 
 if __name__ == '__main__':
