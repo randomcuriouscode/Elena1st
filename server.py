7abe077831f08310473269662b6cae3213958a88
unknown <thyrfa@gmail.com>
2017-11-29 17:52:19 -0500

    Added server to accept client coordinates and call algorithm: broken pathfinding & heuristic

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import flask
 import elena.algo.lawler_paths
+from geopy.distance import vincenty
 
 app = Flask(__name__, static_folder='../static/dist', template_folder='../static')
 
@@ -18,6 +19,7 @@ def route():
     # print(request.get_data())
     # arraydata = str(request.get_data()).replace('&',' ').replace('=',' ').replace('\'',' ').split(" ")
     # print(request.args)
+    nodeStorage = parse("nodeStorage.pickle")
     fromlat = request.args.get('fromlat')
     fromlng = request.args.get('fromlng')
     tolat = request.args.get('tolat')
@@ -25,9 +27,24 @@ def route():
     prefs = request.args.get('route_pref')
     elevation = prefs.get('elevation')
     distance = prefs.get('distance')
-    pathList = get_shortest_paths
-    print("fromlat: "+str(fromlat)+"\nfromlng: "+str(fromlng)+"\ntolat: "+str(tolat)+"\ntolng: "+str(tolng)+"\nflexibility: "+str(flex))
-    return 'callback()'
+    pathList = get_shortest_paths(nodeStorage, fromnode, tonode,
+    bestPath = pathList[0]
+    bestElev = get_elevation(bestPath[0])
+    for path in pathList:
+        elv = get_elevation(path[0])
+        if elv == bestElev:
+            if bestPath[1] < path[1]:
+                bestElev = elv
+                bestPath = path
+        elif elevation = 1:
+            if elv < bestElev:
+                bestElev = elv
+                bestPath = path
+        elif elevation = 2:
+            if elv > bestElev:
+                bestElev = elv
+                bestPath = path
+    return jsonify(List=bestPath[0], distance=bestPath[1], elev=bestElev)
 
 def get_elevation(nodeList):
     elevSum = 0
@@ -39,7 +56,7 @@ def get_elevation(nodeList):
                 elevSum+=difference
         previousNode = node
     return elevSum
-            
+
         
 def callback():
     return 'placeholder'
