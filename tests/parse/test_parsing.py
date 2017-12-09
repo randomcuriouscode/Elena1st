54e896aadcb3767b1fd46e9c35cce763a08acd2a
avaneesh93 <reddyavaneesh@gmail.com>
2017-12-09 08:23:06 -0500

    Parsing only highways

--- tests/parse/test_parsing.py
+++ tests/parse/test_parsing.py
@@ -6,10 +6,10 @@ from elena.parse.parser import *
 class TestParsing(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.nodeStorage = parse("B:/CS520/Elena1st/nodeStorage.pickle")
+        cls.nodeStorage = parse("/Users/avaneesh/amherst")
 
     def test_parseCheck(self):
-        self.assertEqual(len(self.nodeStorage.nodeMap), 45071)
+        self.assertEqual(len(self.nodeStorage.nodeMap), 11187)
 
 
 if __name__ == '__main__':
